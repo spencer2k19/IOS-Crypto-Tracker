2e5fa845f5a18f6008ef5d30b026b654cab80e64
spencer2k19 <hachemeloic@gmail.com>
2023-05-15 09:56:54 +0100

    feature(Coins):saving images locally

--- SwiftUICrypto/Services/CoinImageService.swift
+++ SwiftUICrypto/Services/CoinImageService.swift
@@ -17,13 +17,27 @@ class CoinImageService {
     private var imageSubscription:AnyCancellable?
     
     private let coin:CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin:CoinModel) {
         self.coin = coin
+        imageName = coin.id
        getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            
+            //retrieve image from File Manager
+            image = savedImage
+        } else {
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string:coin.image) else {return}
         
         imageSubscription = NetworkingManager.download(url: url)
@@ -31,8 +45,13 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleData(completion:), receiveValue: {[weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self,
+                let downloadedImage = returnedImage
+                else {return}
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName,
+                                           folderName: self.folderName)
             })
     }
 }
